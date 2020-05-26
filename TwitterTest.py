1bfae2b7c3a9ecdef52b6a64a360c4bf5917b53e
Vivvi <57920864+vivschmidt@users.noreply.github.com>
2020-05-26 18:53:58 +0200

    Create TwitterTest.py

---
+++ TwitterTest.py
@@ -0,0 +1,28 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from kafka import SimpleProducer
+from kafka import KafkaClient
+from kafka import KafkaConsumer
+	
+access_token = ""
+access_token_secret= ""
+consumer_key = ""
+consumer_secret= ""
+	
+class StdOutListener(StreamListener):
+	    def on_data(self, data):
+	       producer.send_messages('homeoffice', data.encode('utf-8'))
+	       print data
+	       return True
+	    def on_error(self, status):
+	       print status
+	
+if __name__ == '__main__':
+	   kafka = KafkaClient('192.168.2.118:9092')
+	   producer = SimpleProducer(kafka)
+	   I = StdOutListener()
+	   auth = OAuthHandler(consumer_key, consumer_secret)
+	   auth.set_access_token(access_token, access_token_secret)
+	   stream = Stream(auth, I)
+	   stream.filter(track='#######')
